f110142601fc32b7750780d1b545241dbbae8142
Anthony Teague <anthony.teague@noxcrew.com>
2023-08-30 19:50:00 +0100

    Make the primary slide more responsive

--- components/show/primary.tsx
+++ components/show/primary.tsx
@@ -3,12 +3,17 @@
 import { useActiveSlide } from "@/utils/hooks"
 import { SLIDES } from "@/utils/slides"
 import { cloneElement } from "react"
+import styles from "@/styles/show.module.scss"
 
 /** The primary (major) slide. */
 export const Primary = () => {
     const activeSlide = useActiveSlide()
 
-    return cloneElement(SLIDES[activeSlide], {
-        primary: true,
-    })
+    return (
+        <div className={styles.primaryContainer}>
+            {cloneElement(SLIDES[activeSlide], {
+                primary: true,
+            })}
+        </div>
+    )
 }
